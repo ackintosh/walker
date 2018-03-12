d6c17405f1c20689761ba9e9638f53e4cefbce12
akihito.nakano <sora.akatsuki@gmail.com>
2018-03-12 22:34:38 +0900

    Implement set command

--- src/main/java/com/github/ackintosh/walker/Main.java
+++ src/main/java/com/github/ackintosh/walker/Main.java
@@ -6,9 +6,13 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
+        HashMap<String, String> values = new HashMap<String, String>();
+
         try {
             ServerSocket listener = new ServerSocket();
             listener.setReuseAddress(true);
@@ -20,8 +24,17 @@ public class Main {
                 InputStream from = socket.getInputStream();
                 OutputStream to = socket.getOutputStream();
                 int receivedByte;
-                while ((receivedByte = from.read()) != -1) {
-                    to.write(receivedByte);
+                byte[] data = new byte[1024];
+                while ((receivedByte = from.read(data)) != -1) {
+                    String receivedString = new String(data, Charset.forName("UTF-8"));
+                    if (receivedString.startsWith("set ")) {
+                        String[] parts = receivedString.split("[\\s]+", 3);
+                        String key = parts[1];
+                        String value = parts[2].substring(0, parts[2].indexOf("\r\n"));
+                        values.put(key, value);
+                    }
+                    // echo
+                    to.write(data);
                 }
             }
         } catch (IOException e) {
